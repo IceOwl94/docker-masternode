92546dda7a513f518a32bf99840f036690415b8a
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-02-25 11:34:19 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Docker setup
-apt-get remove docker docker-engine docker.io
+apt-get remove -y docker docker-engine docker.io
 
 apt-get update
-apt-get upgrade
-apt-get install \
+apt-get upgrade -y
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -17,7 +17,7 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 
 # Docker Compose setup
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
