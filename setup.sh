ddd41e2c07aea0e6c0bb34f1eb3dd01ee81f6cee
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-02-25 11:51:57 +0100

    add swap in setup

--- setup.sh
+++ setup.sh
@@ -22,3 +22,9 @@ sudo apt-get install -y docker-ce
 # Docker Compose setup
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Add swap space
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
