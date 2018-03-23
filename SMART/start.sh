218231183930944a8ddbd3c59d787bdd9f600f23
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-03-23 23:16:09 +0100

    again

--- SMART/start.sh
+++ SMART/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "rpcuser=smartcashrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nport=$PORT\ndaemon=1\nlisten=1\nserver=1\nsmartnode=1\nsmartnodeprivkey=$MNPRVKEY" > /root/.smartcash/smartcash.conf
+echo -e "rpcuser=smartcashrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\ntxindex=1\nport=$PORT\ndaemon=1\nlisten=1\nserver=1\nsmartnode=1\nsmartnodeprivkey=$MNPRVKEY" > /root/.smartcash/smartcash.conf
 
 ./smartcash-1.1.1/bin/smartcashd
 /bin/bash
