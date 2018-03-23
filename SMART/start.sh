93eb0040836482b188ff66339dcfaa59b84627c1
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-03-23 23:14:29 +0100

    again

--- SMART/start.sh
+++ SMART/start.sh
@@ -2,5 +2,5 @@
 
 echo -e "rpcuser=smartcashrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nport=$PORT\ndaemon=1\nlisten=1\nserver=1\nsmartnode=1\nsmartnodeprivkey=$MNPRVKEY" > /root/.smartcash/smartcash.conf
 
-./smartcash-1.1.1/bin/smartcashd/smartcashd
+./smartcash-1.1.1/bin/smartcashd
 /bin/bash
