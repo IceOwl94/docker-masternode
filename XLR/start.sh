0eed38ff7f457945b4a863caf0098f6823f2d2ee
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-03-31 23:14:21 +0200

    try to fix xlr

--- XLR/start.sh
+++ XLR/start.sh
@@ -2,5 +2,5 @@
 
 echo -e "rpcallowip=127.0.0.1\nrpcuser=solarisrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nstaking=0\nserver=1\ndaemon=1\nlisten=1\nport=60020\nmasternode=1\nmasternodeaddr=$MY_IP:60020\nexternalip=$MY_IP:60020\nmasternodeprivkey=$MNPRVKEY" > /root/.solaris/solaris.conf
 
-./solarisd
+./solarisd -daemon
 /bin/bash
