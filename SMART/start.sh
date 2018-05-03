bddf1a18d461d5386afd873028cc9e5fa6fcbcaf
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-05-03 20:36:26 +0200

    fix smart start

--- SMART/start.sh
+++ SMART/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "rpcuser=smartcashrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nport=9678\ndaemon=1\nlisten=1\nserver=1\nsmartnode=1\ntxindex=1\nsmartnodeprivkey=$MNPRVKEY\nexternalip=$MY_IP:9678" > /root/.smartcash/smartcash.conf
+echo -e "rpcuser=smartcashrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nrpcallowip=127.0.0.1\nport=9678\ndaemon=1\nlisten=1\nserver=1\nsmartnode=1\ntxindex=1\nsmartnodeprivkey=$MNPRVKEY\nexternalip=$MY_IP:9678\nmaxconnections=64" > /root/.smartcash/smartcash.conf
 
 ./smartcash-1.1.1/bin/smartcashd
 /bin/bash
