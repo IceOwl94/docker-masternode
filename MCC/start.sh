bd049aa30f04c9a1597d090c983d9cf595d7b2d2
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-05-16 20:30:40 +0200

    .

--- MCC/start.sh
+++ MCC/start.sh
@@ -2,5 +2,5 @@
 
 echo -e "rpcallowip=127.0.0.1\nrpcuser=mccrpc\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nserver=1\ndaemon=1\nlisten=0\nmasternode=1\nmasternodeaddr=$MY_IP:29868\nexternalip=$MY_IP\nbind=MY_IP\nmasternodeprivkey=$MNPRVKEY" > /root/.mcc/mcc.conf
 
-mccd
+./mccd
 /bin/bash
