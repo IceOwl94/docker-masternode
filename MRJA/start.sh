9545d5751475e6969aee2a6fece110a7ebe27ff4
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-05-20 11:27:51 +0200

    fix MRJA

--- MRJA/start.sh
+++ MRJA/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "rpcuser=ganjamasternode\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nrpcallowip=localhost\nport=$PORT\nexternalip=$MY_IP\nserver=1\nlisten=1\ndaemon=1\nlogtimestamps=1\ntxindex=$TX_HASH\nmaxconnections=500\nmnconflock=1\nmasternode=1\nmasternodeaddr=$MY_IP:$PORT\nmasternodeprivkey=$MNPRVKEY\nstake=0\nstaking=0\nseednode=138.197.44.71" > /root/.MRJA/GanjaProject.conf 
+echo -e "rpcuser=ganjamasternode\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nrpcallowip=localhost\nport=$PORT\nexternalip=$MY_IP\nserver=1\nlisten=1\ndaemon=1\nlogtimestamps=1\nmaxconnections=500\nmnconflock=1\nmasternode=1\nmasternodeaddr=$MY_IP:$PORT\nmasternodeprivkey=$MNPRVKEY\nstake=0\nstaking=0\nseednode=138.197.44.71" > /root/.MRJA/GanjaProject.conf 
 
 /usr/local/bin/Ganjad
 /bin/bash
