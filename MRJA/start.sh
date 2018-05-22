4e3d77c5d04ad31f0d5b563ded5d7c030428b5d1
Thomas De Zordi <dezordi.thomas@gmail.com>
2018-05-22 19:21:55 +0200

    new Ganjacoin

--- MRJA/start.sh
+++ MRJA/start.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo -e "rpcuser=ganjamasternode\nrpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\nrpcallowip=localhost\nport=$PORT\nexternalip=$MY_IP\nserver=1\nlisten=1\ndaemon=1\nlogtimestamps=1\nmaxconnections=500\nmnconflock=1\nmasternode=1\nmasternodeaddr=$MY_IP:$PORT\nmasternodeprivkey=$MNPRVKEY\nstake=0\nstaking=0\nseednode=138.197.44.71" > /root/.MRJA/GanjaProject.conf 
+echo -e "rpcuser=$(date +%s | sha256sum | base64 | head -c 32 ;  )\n\
+rpcpassword=$(date +%s | sha512sum | base64 | head -c 32 ;  )\n\
+rpcallowip=localhost\n\
+rpcport=$PORT\n\
+port=11420\n\
+externalip=$MY_IP\n\
+server=1\n\
+listen=1\n\
+daemon=1\n\
+logtimestamps=1\n\
+maxconnections=500\n\
+mnconflock=1\n\
+masternode=1\n\
+masternodeaddr=$MY_IP:$PORT\n\
+masternodeprivkey=$MNPRVKEY\n\
+stake=0\n\
+staking=0\n\
+seednode=138.197.44.71" > /root/.MRJA/GanjaProject.conf 
 
-/usr/local/bin/Ganjad
+./Ganjacoind
 /bin/bash
