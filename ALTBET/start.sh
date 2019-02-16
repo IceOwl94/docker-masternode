d41b38c7ff29bc8bcd723d431d758946e5516e73
io <>
2019-02-16 19:37:59 +0100

    add altbet

---
+++ ALTBET/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\
+rpcallowip=127.0.0.1\n\
+rpcuser=bitgrpc\n\
+rpcpassword=$(date +%s | sha256sum | base64 | head -c 32 ;  )\n\
+server=1\n\
+daemon=1\n\
+listen=1\n\
+masternode=1\n\
+port=$PORT\n\
+masternodeaddr=$MY_IP:$PORT\n\
+externalip=$MY_IP\n\
+masternodeprivkey=$MNPRVKEY" > /root/.altbet/altbet.conf
+
+./altbetd
+/bin/bash
