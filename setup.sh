4bea8c423b8413c31807bee350a831d6bca69ce2
dezordithomas <dezordi.thomas@gmail.com>
2019-03-30 08:31:40 +0100

    test pm2 startup

--- setup.sh
+++ setup.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 
-# Docker setup
-apt-get remove -y docker docker-engine docker.io
-
-apt-get update
-apt-get upgrade -y
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-sudo apt-get install -y docker-ce
-
-# Docker Compose setup
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# Nodejs setup
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
+## Docker setup
+#apt-get remove -y docker docker-engine docker.io
+#
+#apt-get update
+#apt-get upgrade -y
+#apt-get install -y \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#apt-key fingerprint 0EBFCD88
+#add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+#apt-get update
+#sudo apt-get install -y docker-ce
+#
+## Docker Compose setup
+#sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+#
+## Nodejs setup
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
 
 # Install dependencies
 npm i
 
-# Add swap space
-fallocate -l 4G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+# Install pm2
+npm install pm2 -g
 
-# Add startup cron
-(crontab -l ; echo "@reboot cd $(pwd) && /usr/local/bin/docker-compose up -d")| crontab -
+# Setup pm2 startup
+npm run tsc
+pm2 start ./dist/index.js
+pm2 startup
+pm2 save
 
-reboot
+## Add swap space
+#fallocate -l 4G /swapfile
+#chmod 600 /swapfile
+#mkswap /swapfile
+#swapon /swapfile
+#cp /etc/fstab /etc/fstab.bak
+#echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+#
+## Add startup cron
+#(crontab -l ; echo "@reboot cd $(pwd) && /usr/local/bin/docker-compose up -d")| crontab -
+#
+#reboot
