5c52cb206fdd20f8441985658c4d1ae884c5809e
dezordithomas <dezordi.thomas@gmail.com>
2019-07-06 02:06:29 +0200

    update abet

--- wallets/ABET/index.ts
+++ wallets/ABET/index.ts
@@ -8,5 +8,5 @@ export const args: MasternodeArgs = {
   DAEMON_FILE: 'altbetd',
   COIN_PORT: 8322,
   WALLET: 'https://github.com/altbet/abet/releases/download/v1.0.0.0/altbet-v1.0.0.0-ubu1604.tar.gz',
-  COIN_BLOCKS: 'https://github.com/IceOwl94/docker-masternode/releases/download/0.1.0/empty.zip',
+  COIN_BLOCKS: '',
 }
