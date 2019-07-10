d840873ccbc6c9bbb377efec5c340ccdbd98b325
dezordithomas <dezordi.thomas@gmail.com>
2019-07-10 23:11:57 +0200

    update abet

--- wallets/ABET/index.ts
+++ wallets/ABET/index.ts
@@ -7,6 +7,6 @@ export const args: MasternodeArgs = {
   CONFIG_FOLDER: '/root/.altbet',
   DAEMON_FILE: 'altbetd',
   COIN_PORT: 8322,
-  WALLET: 'https://github.com/altbet/abet/releases/download/v1.0.0.0/altbet-v1.0.0.0-ubu1604.tar.gz',
+  WALLET: 'https://github.com/altbet/abet/releases/download/v1.0.0.1/altbet-v1.0.0.1-ubu1604.tar.gz',
   COIN_BLOCKS: '',
 }
