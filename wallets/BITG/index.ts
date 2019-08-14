9bcbb9f5ef3260e2c7213b85551ded70565662c2
io <>
2019-08-14 12:24:27 +0200

    update BITG

--- wallets/BITG/index.ts
+++ wallets/BITG/index.ts
@@ -7,6 +7,6 @@ export const args: MasternodeArgs = {
   CONFIG_FOLDER: '/root/.bitgreen',
   DAEMON_FILE: 'bitgreend',
   COIN_PORT: 9333,
-  WALLET: 'https://github.com/bitgreen/bitgreen/releases/download/v1.3.1/bitgreen-1.3.1-x86_64-linux-gnu.tar.gz',
+  WALLET: 'https://github.com/bitgreen/bitgreen/releases/download/v1.3.2/bitgreen-1.3.2-x86_64-linux-gnu.tar.gz',
   COIN_BLOCKS: 'https://github.com/IceOwl94/docker-masternode/releases/download/0.1.0/bitg-bootsrap-750000.zip',
 }
